48ce9f4eeb10530393728ba3c4ed950656df33de
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-10-12 17:42:05 +0700

    Initial commit

---
+++ QueryResult/Common/DatabaseManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryResult.Common
+{
+    public class DatabaseManager
+    {
+        public string sqlConnection = "Data Source=103.90.248.49;Initial Catalog=BSI_POC;User ID = sa; Password=P@ssw0rd!23#";
+        public SqlCommand cmd;
+        public SqlDataReader dReader;
+        public SqlTransaction trans;
+    }
+}
